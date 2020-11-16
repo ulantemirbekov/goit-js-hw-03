42e9101ee5c2bbd93f571fcfc2f153ab1dd137cf
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-16 20:56:32 +0200

    task-05 solution added

---
+++ task-05.js
@@ -0,0 +1,33 @@
+console.log('Task-05');
+console.log('-------');
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+    
+    const items = [];
+
+    for (const product of arr) {
+        
+        if (prop in product) {
+           items.push(product[prop]);
+        }
+    } 
+    return items;  
+};
+
+// Вызовы функции для проверки работоспособности твоей реализации.
+
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
+
+// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства.
+// Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
