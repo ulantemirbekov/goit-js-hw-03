fcee03e98b0763f3e151eab9de67da218284277b
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-17 23:21:58 +0200

    Update task-04.js

--- task-04.js
+++ task-04.js
@@ -1,40 +1,9 @@
-console.log('Task-04');
-console.log('-------');
-
 const countTotalSalary = function (employees) {
-  
-    const salaries = Object.values(employees);
-    
-    let totalSalary = 0;
 
-    for (const salary of salaries) {
-    totalSalary += salary;
-    }
+  let totalSalary = 0;
 
-    return totalSalary;
+  for (const key in employees) {
+    totalSalary += employees[key];
+  }
+  return totalSalary;
 };
-
-//  Вызовы функции для проверки работоспособности твоей реализации.
-
-console.log(countTotalSalary({})); // 0
-
-console.log(
-  countTotalSalary({
-    mango: 100,
-    poly: 150,
-    alfred: 80,
-  }),
-); // 330
-
-console.log(
-  countTotalSalary({
-    kiwi: 200,
-    lux: 50,
-    chelsy: 150,
-  }),
-); // 400
-
-
-// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.
-// Функция считает общую сумму запрплаты работников и возращает ее.
-// Каждое поле объекта, передаваемого в функцию, имеет вид "имя": "зарплата".
