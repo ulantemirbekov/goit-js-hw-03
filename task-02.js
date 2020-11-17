e7f1a06542c9fc4c7db5e4808697a3443e91bbe3
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-17 22:29:56 +0200

    Update task-02.js

--- task-02.js
+++ task-02.js
@@ -1,22 +1,6 @@
-console.log('Task-02');
-console.log('-------');
-
 const countProps = function (obj) {
     
     const keys = Object.keys(obj);
     const numberOfKeys = keys.length;
     return numberOfKeys;
 };
-
-// Вызовы функции для проверки работоспособности твоей реализации.
- 
-console.log(countProps({})); // 0
-
-console.log(countProps({ name: 'Mango', age: 2 })); // 2
-
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
-
-
-
-// Напиши функцию countProps(obj), считающую кол-во свойств в объекте. 
-// Функция возвращает число - количество свойств.
