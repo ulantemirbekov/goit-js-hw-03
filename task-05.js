e1adad113afc5097a683d6fa8b71e6c5ebadbc16
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-17 22:33:54 +0200

    Update task-05.js

--- task-05.js
+++ task-05.js
@@ -1,6 +1,3 @@
-console.log('Task-05');
-console.log('-------');
-
 const products = [
   { name: 'Радар', price: 1300, quantity: 4 },
   { name: 'Сканер', price: 2700, quantity: 3 },
@@ -20,14 +17,3 @@ const getAllPropValues = function (arr, prop) {
     } 
     return items;  
 };
-
-// Вызовы функции для проверки работоспособности твоей реализации.
-
-console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
-
-console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
-
-console.log(getAllPropValues(products, 'category')); // []
-
-// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства.
-// Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
