adae5625b65a196607fa7561512d6b51e94144bd
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-17 22:23:22 +0200

    Update task-01.js

--- task-01.js
+++ task-01.js
@@ -1,37 +1,18 @@
-console.log('Task-01');
-console.log('-------');
-
 const user = {
   name: 'Mango',
   age: 20,
   hobby: 'html',
   premium: true,
 };
-// console.log(user);
 
-user.mood = 'happy'; // добавляет поле mood со значением 'happy'
-// user['full time']= true;
-// console.log(user);
+user.mood = 'happy';
 
-user.hobby = 'skydiving'; // заменяет значение hobby на 'skydiving'
-// console.log(user);
+user.hobby = 'skydiving'; 
 
-user.premium = false; // заменяет значение premium на false
-// console.log(user);
-
-
-// выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
+user.premium = false; 
 
 const keys = Object.keys(user);
  for (const key of keys) {
   console.log(`${key}: ${user[key]}`);
 }
 
-// const entries = Object.entries(user); 
-
-// for (const entry of entries) {
-//   const key = entry[0];
-//   const value = entry[1];
-
-//   console.log(`${key}: ${value}`);
-// }
